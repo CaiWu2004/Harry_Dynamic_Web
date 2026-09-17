d24273f9754cd69b2b2c76fc2b2dcbf124101617
CaiWu2004 <wcai19820@gmail.com>
2026-09-17 10:05:28 -0400

    accordionpage

--- Week03/starter/comp-lib/src/App.jsx
+++ Week03/starter/comp-lib/src/App.jsx
@@ -1,11 +1,14 @@
-import ButtonPage from './pages/ButtonPage'
+import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
 
 const App = () => {
   return (
     <div className="container mx-auto mt-4">
       <ButtonPage />
+      <hr></hr>
+      <AccordionPage />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
